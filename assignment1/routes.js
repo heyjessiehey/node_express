c68373227489ff351c0309c4c66848486aadef98
heyjessiehey <jessieko608@gmail.com>
2021-02-21 10:40:40 -0500

    moved routing into routes.js and export it

--- assignment1/routes.js
+++ assignment1/routes.js
@@ -1,24 +1,38 @@
-const requestHandler = () => {
+const requestHandler = (req, res) => {
   const url = req.url;
   const method = req.method;
+
   if (url === "/") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
-    res.write("<head><title>My First Page</title><head>");
-    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
     res.write("</html>");
-    res.end();
-  } else if (url === "/users") {
+    return res.end();
+  }
+  if (url === "/users") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write(
       "<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></ul></body>"
     );
     res.write("</html>");
+    return res.end();
+  }
+  if (url === "/create-user" && method === "POST") {
+    // parse incomming data
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody.split("=")[1]);
+    });
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     res.end();
   }
 };
